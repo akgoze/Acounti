a756c8de9ab1ff0610e6acea835c4a667ee55050
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-03-13 17:09:00 +0300

    Added angularfire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'acountiapp',
+    appId: '1:649172511356:web:393dc8612a6233effb77f9',
+    storageBucket: 'acountiapp.appspot.com',
+    apiKey: 'AIzaSyCgAwngznbS_D8_NyPZXOckLIHiiGLteP8',
+    authDomain: 'acountiapp.firebaseapp.com',
+    messagingSenderId: '649172511356',
+    measurementId: 'G-REL7V4ZNKF',
+  },
   production: false
 };
 
