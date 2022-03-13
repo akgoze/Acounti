7822f727325a31b459d44d557fb2e19a7dfcdf86
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-03-13 19:08:16 +0300

    Added button component attributes

--- src/app/ui-kit/button/button.component.ts
+++ src/app/ui-kit/button/button.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'el-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+  @Input() btnType: null | 'submit' | 'button' | 'reset' = null;
+  @Input() btnStyle: null | 'solid' | 'outline' | 'textlink' = null;;
+  @Input() btnSize: null | 'sm' | 'md' | 'lg' = 'md';
+  @Input() btnColor: null | 'orange' | 'yellow' | 'blue' | 'green' | 'pink' = null;
+  @Input() btnDisabled: boolean = false;
+  @Input() btnIcon: string = '';
 
   constructor() { }
 
