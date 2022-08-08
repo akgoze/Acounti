54ea82a334b95c8748f4d052395a02d27aa99c26
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-08-08 10:25:55 +0300

    Added button component.

---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,104 @@
+<template>
+<button 
+  :type="btnType"
+  :class="`btn btn-${btnStyle} btn--${btnColor} btn-${btnSize}`"
+  :disabled="btnDisabled">
+    <slot></slot>
+  </button>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'AppButton',
+    props: {
+      btnType: {
+        type: String,
+        default: 'button'
+      },
+      btnStyle: {
+        type: String,
+        default: 'default'
+      },
+      btnColor: {
+        type: String,
+        default: 'default'
+      },
+      btnSize: {
+        type: String, // lg, md, sm
+        default: 'lg'
+      },
+      btnDisabled: {
+        type: Boolean,
+        default: false
+      },
+      btnIcon: {
+        type: String, // icon-left, icon-right, icon-button
+        default: ''
+      }
+    },
+    computed: {
+      // btnClass() {
+      //   return `btn btn--${this.btnColor} btn--${this.btnSize}`;
+      // }
+    }
+  }
+
+</script>
+
+
+<style lang="scss" scoped>
+  @import "./../../assets/stylesheets/variables";
+  .btn {
+    border: none;
+    background: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    font-size: 48px;
+    padding-left: 16px;
+    padding-right: 16px;
+    &-solid {
+      background: #f5f5f5;
+      color: $dark;
+      border-radius: 16px;
+      font-weight: 800;
+      border: 2px solid $dark;
+    }
+
+    /** BUTTON SIZES  */
+    &-sm {
+      font-size: 16px;
+      line-height: 32px;
+      border-radius: 10px;
+      box-shadow: 0px 2px 0px $dark;
+    }
+    &-md {
+      font-size: 18px;
+      line-height: 42px;
+      border-radius: 14px;
+      box-shadow: 0px 3px 0px $dark;
+    }
+    &-lg {
+      font-size: 21px;
+      line-height: 54px;
+      border-radius: 16px;
+      box-shadow: 0px 4px 0px $dark;
+    }
+    /** BUTTON SIZES  */
+
+
+    /** BUTTON COLORS  */
+    @each $name, $color in $colors {
+      &--#{$name} {
+        background: $color;
+        color: $dark;
+        border-radius: 16px;
+        &:hover {
+          box-shadow: none;
+          background-color: darken($color: $color, $amount: 8%);
+        }
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
-createApp(App).mount('#app')
+import AppButton from "./components/UI/AppButton.vue";
+
+const app = createApp(App);
+
+app.mount('#app');
+app.component('app-button', AppButton);
