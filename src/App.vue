6a9c45361a6003166bd8230a1b37bd7c25484e5a
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-08-08 10:25:26 +0300

    Added global styles

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <app-button :btnType="'button'" :btnStyle="'solid'" :btnColor="'yellow'" :btnSize="'sm'">Button Small</app-button>
+  <app-button :btnType="'button'" :btnStyle="'solid'" :btnColor="'yellow'" :btnSize="'md'">Button Medium</app-button>
+  <app-button :btnType="'button'" :btnStyle="'solid'" :btnColor="'yellow'" :btnSize="'lg'">Button Large</app-button>
+  <hr>
+  <hr>
+  
 </template>
 
 <script>
-
+import AppButton from './components/UI/AppButton.vue';
 export default {
   name: 'App',
   components: {
-    
+    AppButton
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import "./assets/stylesheets/global";
 </style>
