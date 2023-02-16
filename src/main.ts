95dd96cc3a4ad7b54b63781afe925e34b13a7616
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-02-16 15:12:19 +0300

    Updated icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './assets/stylesheets/icons/icons.scss'
 import './assets/stylesheets/global.scss'
 
 createApp(App).mount('#app')
