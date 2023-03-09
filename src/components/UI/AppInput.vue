dc1203ffdf531ef51f6fa567a2f9751f0f5c68fe
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-03-09 15:33:31 +0300

    Added text for input type in AppInput component

--- src/components/UI/AppInput.vue
+++ src/components/UI/AppInput.vue
@@ -14,7 +14,7 @@ export interface Props {
 const onFocus = ref(false)
 
 const props = withDefaults(defineProps<Props>(), {
-  type: '',
+  type: 'text',
   placeholder: '',
   inputmode: 'input',
   icon: null,
