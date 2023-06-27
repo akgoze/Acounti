62b4fa1001420ca4e33b2653866732d09b60b9eb
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-06-27 15:24:15 +0300

    Updated tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'dark',
   theme: {
-    extend: {},
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      ...defaultTheme.screens,
+    },
+    colors: {
+      'yellow': '#ffbd12',
+      'green': '#00c6ae',
+      'orange': '#f95a2c',
+      'blue': '#1947e5',
+      'grey': '#9e9e9e',
+      'dark': '#18191f',
+      'pink': '#ff89bb',
+    },
+
+    extend: {
+      fontFamily: {
+        sans: [
+          'Montserrat',
+          ...defaultTheme.fontFamily.sans
+        ]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({addVariant}) {
+      addVariant('third', '&:nth-child(3)')      
+    })
+  ],
 }
