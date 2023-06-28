a8b1671d4ec63f225d65e5ecdbc3ec3c58171a0b
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-06-28 11:53:59 +0300

    Added checkbox component.

---
+++ src/components/UI/AppCheckbox.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+export interface Props {
+  id: string
+  label: string
+  checked: boolean
+  disabled?: boolean
+  onChange: (event: Event) => void  
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  id: '',
+  label: '',
+  checked: false,
+  disabled: false,
+  onChange: (event: Event) => {},
+})
+
+const checkboxAttributes = computed(function (): Record<string, any> {
+  return {
+    id: props.id,
+    label: props.label,
+    checked: props.checked,
+    disabled: props.disabled,
+    onChange: props.onChange,
+  }
+})
+
+</script>
+
+<template>
+  <div class="app-checkbox">
+    <input type="checkbox" v-bind="checkboxAttributes" />
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
