e2bd0b429a918df66a255f335307023309e7384d
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-06-28 11:54:08 +0300

    Added card component.

---
+++ src/components/UI/AppCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+export interface Props {
+  isHover: boolean,
+  title: string | null,
+  description: string | null,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isHover: false,
+  title: null,
+  description: null,
+})
+
+</script>
+<template>
+  <div :class="`app-card ${isHover && 'app-card--hover'}`">
+    <slot name="prepend" />
+    <h3 v-html="title" v-if="title" />
+    <p v-html="description" v-if="description" />
+    <slot />
+  </div>
+</template>
