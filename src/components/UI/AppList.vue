0383ac973a8a295bbabcabb49d1115d4f799e2f6
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-06-28 11:54:27 +0300

    Added list component.

---
+++ src/components/UI/AppList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+export interface Props {
+  title: string
+  description: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+  description: ''
+})
+
+</script>
+<template>
+  <div class="app-list">
+    <slot name="prepend"></slot>
+    <div class="app-list__content">
+      <h3 class="app-list__title" v-html="title" v-if="title" />
+      <p class="app-list__text"  v-html="description" v-if="description"/>
+    </div>
+
+    <slot name="append"></slot>
+  </div>
+</template>
