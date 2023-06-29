200c903e5ebf8bd27f78f4a260cc5c0109d9a0fd
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-06-29 20:05:56 +0300

    Updated config files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import ViteComponents from 'vite-plugin-components'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ViteComponents(
-      {
-        // relative paths to the directory to search for components.
-        dirs: ['src/components/UI'],
-        // valid file extensions for components.
-        extensions: ['vue'],
-        // search for subdirectories
-        deep: false,
-      }
-    )
+    Components({
+      // relative paths to the directory to search for components.
+      dirs: ["src/components/UI"],
+      // valid file extensions for components.
+      extensions: ["vue"],
+      // search for subdirectories
+      deep: false,
+    }),
   ],
-    
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
