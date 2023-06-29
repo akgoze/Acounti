89951a9ada07b9d27ed39e0b03c3b1728189dde6
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-06-29 19:14:09 +0300

    Added main component library.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteComponents from 'vite-plugin-components'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteComponents(
+      {
+        // relative paths to the directory to search for components.
+        dirs: ['src/components/UI'],
+        // valid file extensions for components.
+        extensions: ['vue'],
+        // search for subdirectories
+        deep: false,
+      }
+    )
+  ],
+    
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
